2e0e8ad561a83887f0e34589d5dd417543330012
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-03-01 15:27:53 +0100

    refactor: change hooks logic

--- src/components/ExperienceCanvas/ExperienceCanvas.jsx
+++ src/components/ExperienceCanvas/ExperienceCanvas.jsx
@@ -2,105 +2,88 @@ import './ExperienceCanvas.scss'
 import chapterOneData from '../../assets/chapterOne.json'
 import { useEffect, useState } from 'react'
 
-export default function ExperienceCanvas() {
+export const ExperienceCanvas = () => {
+  const scriptData = chapterOneData
   // Local state
-  const [displayUi, setDisplayUi] = useState(true)
+  const [displayUi, setDisplayUi] = useState(false)
   const [sceneIndex, setSceneIndex] = useState(0)
+  const [isVoiceOver, setIsVoiceOver] = useState(false)
+  const [spotIndex, setSpotIndex] = useState(0)
   const [textIndex, setTextIndex] = useState(0)
 
-  const [spot, setSpot] = useState(chapterOneData[sceneIndex])
-  const [voiceover, setVoiceover] = useState(spot.voiceover)
-  const [speaker, setSpeaker] = useState(voiceover[0].emitter)
-
-  const hasMore = textIndex < voiceover.length - 1
-
-  // Init the first scene
   useEffect(() => {
-    initScene(0)
-    console.log('initial scene init')
-  }, [])
-
-  const initScene = (dataSceneIndex) => {
-    setSceneIndex(dataSceneIndex)
-    setSpot(chapterOneData[sceneIndex])
-    setVoiceover(spot.voiceover)
-    setSpeaker(voiceover[0].emitter)
-    setTextIndex(0)
-    setDisplayUi(true)
-  }
+    if (scriptData[sceneIndex].voiceover.length > 0) {
+      setIsVoiceOver(true)
+      setDisplayUi(true)
+    }
+  }, [sceneIndex])
 
   // Change the current scene and reset UI state
   const changeScene = (sceneIndex) => {
-    resetUiState()
+    setSpotIndex(0)
+    setTextIndex(0)
+    setDisplayUi(false)
     setSceneIndex(sceneIndex)
-    initScene(sceneIndex)
   }
-
-  useEffect(() => {
-    console.log(spot)
-  }, [spot])
 
   // Change the current spot and reset UI state
   const goToSpot = (spotIndex) => {
-    setDisplayUi(true)
-    setSpot(chapterOneData[sceneIndex].spots[spotIndex])
-    setVoiceover(chapterOneData[sceneIndex].spots[spotIndex].voiceover)
-    setSpeaker(voiceover[0].emitter)
+    setSpotIndex(spotIndex)
     setTextIndex(0)
+    setIsVoiceOver(false)
+    setDisplayUi(true)
   }
 
   // Show the next text in the voiceover array
   const showMore = () => {
     setTextIndex(textIndex + 1)
-    setSpeaker(voiceover[textIndex + 1].emitter)
   }
 
-  // Reset UI state
-  const resetUiState = () => {
-    setDisplayUi(false)
-    setTextIndex(0)
-  }
+  const getTextEmitter = () =>
+    !isVoiceOver ? scriptData[sceneIndex].spots[spotIndex].emitter : 'narrator'
+
+  const hasMore = () => (isVoiceOver ? hasMoreIntroText() : hasMoreSpotText())
+
+  const getIntroText = () => scriptData[sceneIndex].voiceover && scriptData[sceneIndex].voiceover[textIndex].text
+  const hasMoreIntroText = () => scriptData[sceneIndex].voiceover.length > textIndex + 1
+
+  const getSpotText = () => scriptData[sceneIndex].spots[spotIndex].voiceoverspot[textIndex].text
+  const hasMoreSpotText = () =>
+    scriptData[sceneIndex].spots[spotIndex].voiceoverspot.length > textIndex + 1
 
   return (
     <section className="ExperienceCanvas">
       <div className="buttons">
         <h2>CHOISIR UNE SCÈNE:&nbsp;</h2>
-        <button onClick={() => changeScene(0)}>Cercles mégalithiques</button>
-        <button onClick={() => changeScene(1)}>Ancienne mine de phosphate</button>
+        {scriptData.map((scene, index) => (
+          <button onClick={() => changeScene(index)}>{scene.name}</button>
+        ))}
       </div>
       <hr />
       <div>
         <h2>CHOISIR UN SPOT:&nbsp;</h2>
-        {sceneIndex === 0 && (
-          <button className="spot" onClick={() => goToSpot(0)}>
-            Cercles de pierre
+        {scriptData[sceneIndex].spots.map((spot) => (
+          <button className="spot" onClick={() => goToSpot(spot.index)}>
+            {spot.label}
           </button>
-        )}
-        {sceneIndex === 1 && (
-          <>
-            <button className="spot" onClick={() => goToSpot(0)}>
-              Outils abandonnés
-            </button>
-            <button className="spot spot--2" onClick={() => goToSpot(1)}>
-              Entrée de la mine
-            </button>
-          </>
-        )}
+        ))}
       </div>
       <canvas id="webgl"></canvas>
       {displayUi && (
         <div className="dialogue">
-          {speaker === 'narrator' && <h2 className="narrator">Le narrateur</h2>}
-          {speaker === 'innerVoice' && <h2 className="narrator narrator--innerVoice">Une voix</h2>}
+          {getTextEmitter() === 'narrator' ? (
+            <h2 className="narrator">Le narrateur</h2>
+          ) : (
+            <h2 className="narrator narrator--innerVoice">Une voix</h2>
+          )}
           <div>
-            <p>{voiceover[textIndex].text}</p>
-            {hasMore && (
+            <p>{isVoiceOver ? getIntroText() : getSpotText()}</p>
+            {hasMore() ? (
               <button className="more" onClick={showMore}>
                 Suite
               </button>
-            )}
-            {!hasMore && (
-              <button className="more" onClick={resetUiState}>
+            ) : (
+              <button className="more" onClick={() => setDisplayUi(false)}>
                 Fermer
               </button>
             )}
